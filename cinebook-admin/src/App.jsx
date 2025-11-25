a4673787bb8ed87d9901e869c539a18a7128aa62
Nkugavarathan <kugavarathan28@gmail.com>
2025-11-25 10:29:28 +0530

    Update App

--- cinebook-admin/src/App.jsx
+++ cinebook-admin/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         {/* Admin Dashboard */}
-        <Routes
+        <Route
           path="/admin/dashboard"
           element={
             <ProtectedAdminRoute>

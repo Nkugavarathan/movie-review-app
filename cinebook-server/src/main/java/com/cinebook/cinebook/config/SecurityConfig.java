13028dc765900e48193807c87680a78ea430ad78
Nkugavarathan <kugavarathan28@gmail.com>
2025-11-26 18:26:11 +0530

    Update SecurityConfig

--- cinebook-server/src/main/java/com/cinebook/cinebook/config/SecurityConfig.java
+++ cinebook-server/src/main/java/com/cinebook/cinebook/config/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig {
         configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.
+        registerCorsConfiguration("/**", configuration);
 
         return source;
     }
